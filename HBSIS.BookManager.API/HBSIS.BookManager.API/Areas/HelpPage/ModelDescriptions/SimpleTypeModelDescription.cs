1c634fa0e9ba65eb899cc96f19f3f493e11eee5d
Gabriel Augusto <gabrielgst56@hotmail.com>
2018-09-15 13:14:52 -0300

    Clear repository

--- HBSIS.BookManager.API/HBSIS.BookManager.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace HBSIS.BookManager.API.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- HBSIS.BookManager.API/HBSIS.BookManager.API/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HBSIS.BookManager.API.WebApiApplication" Language="C#" %>
